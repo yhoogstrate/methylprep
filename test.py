bc58a75b2554c7b09e8676dd63171fae3bfdd669
yhoogstrate <y.hoogstrate@gmail.com>
2024-01-26 14:04:33 +0100

    patch

--- test.py
+++ test.py
@@ -22,6 +22,18 @@ wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM6379nnn/GSM6379997/suppl/GSM637
 gunzip cache/GSM6379997_203927450093_R01C01_Red.idat.gz
 gunzip cache/GSM6379997_203927450093_R01C01_Grn.idat.gz
 
+
+
+rm cache/GSM6379998_203927450107_R07C01_Grn.idat.gz
+rm cache/GSM6379998_203927450107_R07C01_Red.idat.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM6379nnn/GSM6379998/suppl/GSM6379998_203927450107_R07C01_Grn.idat.gz --output-document=cache/GSM6379998_203927450107_R07C01_Grn.idat.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM6379nnn/GSM6379998/suppl/GSM6379998_203927450107_R07C01_Red.idat.gz --output-document=cache/GSM6379998_203927450107_R07C01_Red.idat.gz
+
+gunzip cache/GSM6379998_203927450107_R07C01_Grn.idat.gz
+gunzip cache/GSM6379998_203927450107_R07C01_Red.idat.gz
+
+
 python
 
 """
@@ -31,6 +43,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 from pymetharray.files import create_sample_sheet
 ss = create_sample_sheet('cache/', output_file='cache/samplesheet.csv', output_path = ".")
+for sample in ss:
+    print(" - Sample: "+str(sample))
 
 
 from pymetharray.processing.pipeline import run_pipeline_ss
@@ -48,5 +62,4 @@ run_pipeline_ss(ss,
     do_save_noob = False
     
     )
-
 
